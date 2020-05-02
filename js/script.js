7b883f3a7d87a480271aad2eecf4d120964a6037
Mateusz Mikolajczyk <mikolajczykmateusz1993@gmail.com>
2020-05-02 17:03:56 +0000

    Add prototype of layout

--- js/script.js
+++ js/script.js
@@ -1,8 +1,34 @@
 const hamburger = document.querySelector('#hamburger');
-const navigation = document.querySelector('#navigation');
+const sidenav = document.querySelector('.sidenav');
+const close = document.querySelector('#close');
+const container = document.querySelector('#container');
 
-console.log(hamburger);
 
-hamburger.addEventListener('click', function () {
-  navigation.classList.toggle('hamburger');
+
+hamburger.addEventListener('click', function(){
+  sidenav.classList.add('visible');
+  container.classList.add('container-active');
+});
+
+close.addEventListener('click', function(){
+  sidenav.classList.remove('visible');
+  container.classList.remove('container-active');
+
+});
+
+
+//mobile
+const toggleMobileTrigger = document.querySelector('#toggleMobileTrigger');
+const toggleDropdownTrigger = document.querySelector('#dropdown-trigger');
+const navMobile = document.querySelector('#nav-mobile');
+const dropdown = document.querySelector('#dropdown');
+
+toggleMobileTrigger.addEventListener('click', function(){
+  console.log('dupa');
+  navMobile.classList.toggle('active-mobile');
+});
+
+toggleDropdownTrigger.addEventListener('click', function(){
+  console.log('pipa');
+  dropdown.classList.toggle('active-dropdown');
 });
