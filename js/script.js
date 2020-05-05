48fe67423051cdf345e0049427047ed2d6ef22c4
Mateusz Mikolajczyk <mikolajczykmateusz1993@gmail.com>
2020-05-05 18:52:51 +0000

    Add all sections(layout need fixing

--- js/script.js
+++ js/script.js
@@ -1,34 +1,51 @@
-const hamburger = document.querySelector('#hamburger');
-const sidenav = document.querySelector('.sidenav');
-const close = document.querySelector('#close');
-const container = document.querySelector('#container');
+const hamburger = document.querySelector("#hamburger");
+const sidenav = document.querySelector(".sidenav");
+const close = document.querySelector("#close");
+const container = document.querySelector("#container");
 
-
-
-hamburger.addEventListener('click', function(){
-  sidenav.classList.add('visible');
-  container.classList.add('container-active');
+hamburger.addEventListener("click", function () {
+  sidenav.classList.add("visible");
+  container.classList.add("container-active");
 });
 
-close.addEventListener('click', function(){
-  sidenav.classList.remove('visible');
-  container.classList.remove('container-active');
-
+close.addEventListener("click", function () {
+  sidenav.classList.remove("visible");
+  container.classList.remove("container-active");
 });
-
 
 //mobile
-const toggleMobileTrigger = document.querySelector('#toggleMobileTrigger');
-const toggleDropdownTrigger = document.querySelector('#dropdown-trigger');
-const navMobile = document.querySelector('#nav-mobile');
-const dropdown = document.querySelector('#dropdown');
+const toggleMobileTrigger = document.querySelector("#toggleMobileTrigger");
+const toggleDropdownTrigger = document.querySelector("#dropdown-trigger");
+const navMobile = document.querySelector("#nav-mobile");
+const dropdown = document.querySelector("#dropdown");
 
-toggleMobileTrigger.addEventListener('click', function(){
-  console.log('dupa');
-  navMobile.classList.toggle('active-mobile');
+toggleMobileTrigger.addEventListener("click", function () {
+  console.log("dupa");
+  navMobile.classList.toggle("active-mobile");
 });
 
-toggleDropdownTrigger.addEventListener('click', function(){
-  console.log('pipa');
-  dropdown.classList.toggle('active-dropdown');
+toggleDropdownTrigger.addEventListener("click", function () {
+  console.log("pipa");
+  dropdown.classList.toggle("active-dropdown");
+});
+
+window.addEventListener("resize", function (event) {
+  sidenav.classList.remove("visible");
+  container.classList.remove("container-active");
+  navMobile.classList.remove("active-mobile");
+});
+
+
+//jquery scroll sections
+
+$("a[href^='#']").click(function (e) {
+  e.preventDefault();
+
+  var position = $($(this).attr("href")).offset().top;
+
+  $("body, html").animate(
+    {
+      scrollTop: position,
+    } /* speed */
+  );
 });
