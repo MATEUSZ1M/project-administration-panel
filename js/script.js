c532e4c5ac06d354db7df3699fa1fff99d741b55
Mateusz Mikolajczyk <mikolajczykmateusz1993@gmail.com>
2020-05-10 18:02:40 +0000

    Build 0.1

--- js/script.js
+++ js/script.js
@@ -5,15 +5,14 @@ const dropDownTrigger = document.querySelector('.drop-down-trigger');
 const dropDown = document.querySelector('.light-list');
 const navItems = document.querySelectorAll('.navigation li');
 
-
 console.log(navItems);
 // hamburger
-function toggleMenu () {
+function toggleMenu() {
   menu.classList.toggle('visible');
   container.classList.toggle('container-active');
 }
 //dropdown
-function toggleDropdown () {
+function toggleDropdown() {
   dropDown.classList.toggle('visible-dropdown');
 }
 
@@ -28,32 +27,32 @@ function closeModal() {
 }
 //open modal
 function openModal(modal) {
-  document.querySelectorAll('#overlay > *').forEach(function(modal) {
+  document.querySelectorAll('#overlay > *').forEach(function (modal) {
     modal.classList.remove('show');
   });
   document.querySelector('#overlay').classList.add('show');
   document.querySelector(modal).classList.add('show');
 }
 //clode modal after clicking on btn
-document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
-  btn.addEventListener('click', function(e) {
+document.querySelectorAll('#overlay .js--close-modal').forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
     e.preventDefault();
     closeModal();
   });
 });
 //close modal after clicking on overlay
-document.querySelector('#overlay').addEventListener('click', function(e) {
-  if(e.target === this) {
+document.querySelector('#overlay').addEventListener('click', function (e) {
+  if (e.target === this) {
     closeModal();
   }
 });
 //close modal after clicking esc key
-document.addEventListener('keyup', function(e) {
-  if(e.keyCode === 27) {
+document.addEventListener('keyup', function (e) {
+  if (e.keyCode === 27) {
     closeModal();
   }
 });
-//toogle menu after click on 
+//toogle menu after click on
 for (let hamburgerTrigger of hamburger) {
   console.log(hamburgerTrigger);
 
@@ -66,20 +65,18 @@ dropDownTrigger.addEventListener('click', function () {
   toggleDropdown();
 });
 // clear menu after selecting section
-for(let navItem of navItems){
-  navItem.addEventListener('click', function(){
+for (let navItem of navItems) {
+  navItem.addEventListener('click', function () {
     console.log(navItem);
-    if(navItem !== dropDownTrigger) {
+    if (navItem !== dropDownTrigger) {
       clearMenu();
     }
   });
- 
- 
 }
 
 window.addEventListener('resize', function (event) {
   clearMenu();
-  closeModal();
+  // closeModal();
 });
 
 //jquery scroll sections
@@ -96,14 +93,50 @@ $('a[href^="#"]').click(function (e) {
   );
 });
 
-
 const modalTriggers = document.querySelectorAll('.modal-trigger');
 for (let modalTrigger of modalTriggers) {
-
   const dataTag = modalTrigger.getAttribute('data-tag');
 
-  modalTrigger.addEventListener('click', function() {
+  modalTrigger.addEventListener('click', function () {
     openModal('#' + dataTag);
   });
 }
 
+//
+
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var chart = new Chart(ctx, {
+  // 1
+  type: 'bar',
+  data: {
+    // 2
+    labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
+    // 3
+    datasets: [
+      {
+        // 4
+        label: 'Signups',
+        // 5
+        backgroundColor: '#8DBEC8',
+        borderColor: '#8DBEC8',
+        // 6
+        data: [52, 51, 41, 94, 26, 6, 72, 9, 21, 88],
+      },
+      {
+        label: 'FTD',
+        backgroundColor: '#F29E4E',
+        borderColor: '#F29E4E',
+        data: [6, 72, 1, 0, 47, 11, 50, 44, 63, 76],
+      },
+      {
+        label: 'Earned',
+        backgroundColor: '#71B374',
+        borderColor: '#71B374',
+        data: [59, 49, 68, 90, 67, 41, 13, 38, 48, 48],
+        // 7
+        hidden: true,
+      },
+    ],
+  },
+});
